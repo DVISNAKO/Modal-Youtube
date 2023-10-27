07a89b94ea9a7a2f268072acb22cc3998a67f31a
Dima <dmitrijs.visnakovs@inbox.lv>
2023-10-27 13:19:27 +0300

    modal

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [modal, setModal] = useState(false);
+
+  useEffect(() => {
+    setModal(false);
+
+    const timer = setTimeout(() => {
+      setModal(true);
+    }, 3000);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      {!modal ? (
+        <div className="btn" onClick={() => setModal(!modal)}>
+          <button>OPEN</button>
+        </div>
+      ) : (
+        <div className="modal">
+          <div className="close" onClick={() => setModal(!modal)}>
+            X
+          </div>
+          <div className="content">MY MODAL</div>
+        </div>
+      )}
     </div>
   );
 }
